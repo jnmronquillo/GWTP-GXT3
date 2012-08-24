81e8e2d3024ceda56eb4013d2c998858686536c4
Juan Manuel <poseidonjm@gmail.com>
2012-08-23 20:04:06 -0500

    fixed for compile-> mvn gwt:compile and mvn compile war:war

---
+++ tab-example/src/main/java/com/example/client/Messages.java
@@ -0,0 +1,170 @@
+package com.example.client;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	D:/juan/git-repos/gwtp-gxt3-example/tab-example/src/main/resources/com/example/client/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Add".
+   * 
+   * @return translated "Add"
+   */
+  @DefaultMessage("Add")
+  @Key("add")
+  String add();
+
+  /**
+   * Translated "Add Colaborador".
+   * 
+   * @return translated "Add Colaborador"
+   */
+  @DefaultMessage("Add Colaborador")
+  @Key("addColaborador")
+  String addColaborador();
+
+  /**
+   * Translated "Age".
+   * 
+   * @return translated "Age"
+   */
+  @DefaultMessage("Age")
+  @Key("age")
+  String age();
+
+  /**
+   * Translated "Cancel".
+   * 
+   * @return translated "Cancel"
+   */
+  @DefaultMessage("Cancel")
+  @Key("cancel")
+  String cancel();
+
+  /**
+   * Translated "Code".
+   * 
+   * @return translated "Code"
+   */
+  @DefaultMessage("Code")
+  @Key("code")
+  String code();
+
+  /**
+   * Translated "Delete".
+   * 
+   * @return translated "Delete"
+   */
+  @DefaultMessage("Delete")
+  @Key("delete")
+  String delete();
+
+  /**
+   * Translated "Are you sure to delete selected records?".
+   * 
+   * @return translated "Are you sure to delete selected records?"
+   */
+  @DefaultMessage("Are you sure to delete selected records?")
+  @Key("deleteAllConfirm")
+  String deleteAllConfirm();
+
+  /**
+   * Translated "Delete Colaborador".
+   * 
+   * @return translated "Delete Colaborador"
+   */
+  @DefaultMessage("Delete Colaborador")
+  @Key("deleteColaborador")
+  String deleteColaborador();
+
+  /**
+   * Translated "Are you sure to delete {0}?".
+   * 
+   * @return translated "Are you sure to delete {0}?"
+   */
+  @DefaultMessage("Are you sure to delete {0}?")
+  @Key("deleteConfirm")
+  String deleteConfirm(String arg0);
+
+  /**
+   * Translated "Deleted successful".
+   * 
+   * @return translated "Deleted successful"
+   */
+  @DefaultMessage("Deleted successful")
+  @Key("deleteSuccessful")
+  String deleteSuccessful();
+
+  /**
+   * Translated "Edit".
+   * 
+   * @return translated "Edit"
+   */
+  @DefaultMessage("Edit")
+  @Key("edit")
+  String edit();
+
+  /**
+   * Translated "Edit Colaborador".
+   * 
+   * @return translated "Edit Colaborador"
+   */
+  @DefaultMessage("Edit Colaborador")
+  @Key("editColaborador")
+  String editColaborador();
+
+  /**
+   * Translated "First Name".
+   * 
+   * @return translated "First Name"
+   */
+  @DefaultMessage("First Name")
+  @Key("firstName")
+  String firstName();
+
+  /**
+   * Translated "User Information".
+   * 
+   * @return translated "User Information"
+   */
+  @DefaultMessage("User Information")
+  @Key("infoUser")
+  String infoUser();
+
+  /**
+   * Translated "Last Name".
+   * 
+   * @return translated "Last Name"
+   */
+  @DefaultMessage("Last Name")
+  @Key("lastName")
+  String lastName();
+
+  /**
+   * Translated "Save".
+   * 
+   * @return translated "Save"
+   */
+  @DefaultMessage("Save")
+  @Key("save")
+  String save();
+
+  /**
+   * Translated "Saved successful".
+   * 
+   * @return translated "Saved successful"
+   */
+  @DefaultMessage("Saved successful")
+  @Key("saveSuccessful")
+  String saveSuccessful();
+
+  /**
+   * Translated "RequestFactory Grid Example".
+   * 
+   * @return translated "RequestFactory Grid Example"
+   */
+  @DefaultMessage("RequestFactory Grid Example")
+  @Key("titlePanel")
+  String titlePanel();
+}
